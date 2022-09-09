01b9799cdacb513cd46d4265f8aca69e54c1d3f0
promhze <promiseigbosonu@gmail.com>
2022-09-08 18:15:09 -0700

    my program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- this prints a sentence
+ * sentence: displays programming as the lin
+ *
+ * Return: will always return zero
+ */
+int main(void)
+{
+	/*declarations*/
+	char set[] = "\"Programming is like building a multilingual puzzle\n";
+
+	/*statements*/
+	puts(set);
+	return (0);
+}
