8a73ebcbcb50ca52ed329cda6373c0e18b059d67
promhze <promiseigbosonu@gmail.com>
2022-09-08 21:22:46 -0700

    God Pls

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 /**
- * main - this dispalys the ouput
+ * main - displays the output
  *
  * Return: always return zero
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
+	/*declarations*/
+	char a;
+	int b;
+	long int c;
+	long long int d;
 	float e;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(d));
-	printf("Size of an int: %d byte(s)\n", sizeof(a));
-	printf("Size of a long int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a float: %d bytes(s)\n", sizeof(e));
+	/*statement*/
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d bytes(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
