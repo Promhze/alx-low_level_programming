e1150b56d42f53437dd8223569cbc6f83da477e0
promhze <promiseigbosonu@gmail.com>
2022-09-09 20:53:46 -0700

    confused

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,11 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-			if (digit1 == 9 && digit2 == 9)
-				continue;
-			putchar(',');
-			putchar(',');
+			if (digit1 == 9 || digit2 == 9)
+			{
+				putchar(',');
+				putchar(',');
+			}
 		}
 	}
 	putchar('\n');
