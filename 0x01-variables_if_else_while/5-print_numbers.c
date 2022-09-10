989915e0af25c36e4a566050f8f3e3abcade9b24
promhze <promiseigbosonu@gmail.com>
2022-09-09 17:28:32 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - display output
+ *
+ * Return: always return zero
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a > 10; a++)
+	{
+		printf("%d", a);
+
+	printf("\n");
+
+	}
+	return (0);
+}
