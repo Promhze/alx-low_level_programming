0d2f7249c68a367dc91388d56868eaacc404c053
promhze <promiseigbosonu@gmail.com>
2022-09-13 19:13:21 -0700

    return either 0 or 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - main entey point
+ *
+ * @c: charcater set
+ * Return:1 if true, 0 if false
+ */
+
+int _islower(int c)
+{	(c >= 'a' & c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
