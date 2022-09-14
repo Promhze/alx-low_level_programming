3a51552a755bb3d612e798ca0dee8415eb0ce8c0
promhze <promiseigbosonu@gmail.com>
2022-09-13 20:44:14 -0700

    print the last digits of an interger

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "6-abs.c"
+
+/**
+ * print_last_digit - this prints the last digit
+ *@n: sets the vakue if the number
+ *
+ * Return: returns 0
+ */
+
+int print_last_digit(int n)
+{
+	_putchar(_abs(n % 10) + '0');
+	return (_abs(n % 10) + '0');
+}
