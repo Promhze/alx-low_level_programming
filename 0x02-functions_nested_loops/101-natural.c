bebcd5f10696ff407e9e9e1aadb9dfb271a598aa
promhze <promiseigbosonu@gmail.com>
2022-09-14 15:01:50 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - will print naturalnumbers
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+
+	}
+	printf("%d\n", b);
+	return (0);
+}
