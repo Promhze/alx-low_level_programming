458e001183512cfca5a448e463ecb0bac7d59011
promhze <promiseigbosonu@gmail.com>
2022-09-13 21:51:27 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,13 @@
 
 /**
  * add - add two intergers
- * @n1: first intergers
- * @n2: 2nd interger
+ * @c: first intergers
+ * @d: 2nd interger
  *
  * Return: returns the resukt of the addition
  */
 
-int add(int n1, int n2)
+int add(int c, int d)
 {
-	int res = n1 + n2;
-
-	return (res);
+	return (c + d);
 }
