e786b143888ff9d91c536662a0414e1c5f151785
promhze <promiseigbosonu@gmail.com>
2022-09-15 05:27:30 -0700

    multuplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - will myltiply two intergers
+ * @a: first interger
+ * @b: second interger
+ *
+ * Return: will return a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
