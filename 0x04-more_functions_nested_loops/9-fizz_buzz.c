e9edc3cc07def3b857f911b7cd7f8e6f45e4166f
promhze <promiseigbosonu@gmail.com>
2022-09-15 15:41:34 -0700

    printfizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * i: stores file.
+ * Return: a number
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 0; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			if (a % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (a % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d", i);
+			}
+			if (i != 100)
+			{
+				printf(" ");
+			}
+			printf("\n");
+			return (0);
+		}
+	}
+}
+
