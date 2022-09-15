ad69779f82e483174d7f6d02d774463977c5c4e2
promhze <promiseigbosonu@gmail.com>
2022-09-14 17:37:16 -0700

    pushing it

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,18 @@
-include "main.h"
+#include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - displays intergers
+ *
+ * Return: always return 0;
  */
+
 int main(void)
 {
-
 	int i;
 
-	i = 0; /*this assigns variables*/
-	positive_or_negative(i); /*determies whether the interger*/
+	i = 0; /*assigns value to the interger*/
+	positive_or_negative(i);
 
 	return (0);
 }
+
