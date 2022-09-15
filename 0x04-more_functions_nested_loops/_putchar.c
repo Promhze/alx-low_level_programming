697b1c23f1f6258f99e3aec96a46a247dde0c254
promhze <promiseigbosonu@gmail.com>
2022-09-15 11:52:59 -0700

    push

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c
+ * @c: input character
+ *
+ * Return: return 1 if successful
+ * if any error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
