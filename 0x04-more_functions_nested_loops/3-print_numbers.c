6b7f9c7f78eeb1ca22671af3d720e5f62ab459e5
promhze <promiseigbosonu@gmail.com>
2022-09-15 06:55:53 -0700

    pushing it now

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,12 @@
 /**
  * print_numbers - will display numbers
  *
- * Return: returns nothing or void
  */
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c + '0');
 	}
