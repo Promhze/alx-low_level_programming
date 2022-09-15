b7a1a334c012fa815e4c2a6fba6ed96ca32916b5
promhze <promiseigbosonu@gmail.com>
2022-09-14 18:15:24 -0700

    2nd main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,3 +7,16 @@
  */
 
 int main(void)
+{
+	int i;
+	
+	printf("Infinite loop incoming :(\n");
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+
+	/*}*/printf("Infinite loop avoided! \\o/\n");
+	
+	return (0);
+}
