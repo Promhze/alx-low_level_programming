6f73511e720a45c922d3950e0c4ab4b3236d20be
promhze <promiseigbosonu@gmail.com>
2022-09-19 16:35:35 -0700

    i am number 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * puts_half - prints half of a string
@@ -7,4 +8,16 @@
 
 void puts_half(char *str)
 {
+	int a;
+	int oE = 0;
 
+	if (_strlen(str) % 2 != 0)
+	{
+		oE += 1;
+	}
+	for (a = (_strlen(str) + oE) / 2; a < _strlen(str); a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
