6385ea1c5708ac9c4500d7674ad563909eb16abe
promhze <promiseigbosonu@gmail.com>
2022-09-19 08:21:52 -0700

    a new lin string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: input string
+ */
+
+void puts(char *str)
+{
+	int num = 0;
+
+	while (num >= 0)
+	{
+		if (str[num] == '\0')
+		{ 
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[num]);
+		num++;
+	}
+}
