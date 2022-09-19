b8b2f970d0105aa7f2b053fc0390ecd9fdbdfd4e
promhze <promiseigbosonu@gmail.com>
2022-09-19 15:22:10 -0700

    i am number 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,6 +4,27 @@
  * rev_string - reverses a string
  * @s: input string
  */
+
 void rev_string(char *s)
 {
-	int c = 
+	int number = 0, a, b;
+	char *str, num;
+
+	while (number >= 0)
+	{
+		if (s[number] == '\0')
+			break;
+		number++;
+	}
+	str = s;
+
+	for (a = 0; a < (number - 1); a++)
+	{
+		for (b = a + 1; b > 0; b--)
+		{
+			num = *(str + b);
+			*(str + b) = *(str + (b - 1));
+			*(str + (b - 1)) = num;
+		}
+	}
+}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,24 +7,4 @@
 
 void puts_half(char *str)
 {
-	int num = 0, letter = 0, n;
 
-	while (str[num++])
-	{
-		letter++;
-	}
-	if ((letter % 2) == 0)
-	{
-		n = letter / 2;
-	}
-	else
-	{
-		n = (letter + 1) / 2;
-	}
-
-	for (num = n; num < letter; num++)
-	{
-		_putchar(str[num]);
-	}
-	_putchar('\n');
-}
