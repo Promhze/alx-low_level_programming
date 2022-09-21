469fe39e6a6cd0d96305920be3c2333491dfc6dc
promhze <promiseigbosonu@gmail.com>
2022-09-21 12:54:34 -0700

    all in one

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 #include <string.h>
 
 /**
@@ -8,7 +8,7 @@
  * Return: returns an interger
  */
 
-char _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int compare;
 	
