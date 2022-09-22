cd9c0f486588f29ef5ad54c40e00b73fce121661
promhze <promiseigbosonu@gmail.com>
2022-09-22 13:49:10 -0700

    all in one

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * leet - leet function
+ * leet - function
  * @s: parameter s
  * Return: a string
  */
 
 char *leet(char *s)
 {
-	char arrg[] = "a4A4e3E3o0O0t7T7l1L1"
+	char arrg[] = "a4A4e3E3o0O0t7T7l1L1";
 	int a;
 	int b;
 
