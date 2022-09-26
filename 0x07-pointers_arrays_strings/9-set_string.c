339a5b8d63bb5f0240ccb7489847b66b153f1620
Promhze <Promiseigbosonu@gmail.com>
2022-09-26 15:40:17 +0100

    diagram

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to char
+ * @s: inut string
+ * @to: character directions
+ *
+ * Return: always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
