a723012b4c985ed19b912ade99d4a76404b18d0e
Promhze <Promiseigbosonu@gmail.com>
2022-09-26 23:35:14 +0100

    all in one

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,9 +16,11 @@ char *_strpbrk(char *s, char *accept)
 		for (b = 0; *(accept + b) != '\0'; b++)
 		{
 			if (*(s + a) == *(accept + b))
+			{
 				return (s + a);
+			}
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
 
